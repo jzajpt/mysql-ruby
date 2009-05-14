23b846bea98ee4d09bca6aa8fd370028d8a330f9
Per Christian B. Viken <perchr@northblue.org>
2009-05-14 18:05:02 +0800

    increase indenting to match the rest of the file
    Signed-off-by: Hector E. Gomez Morales <hectoregm@gmail.com>

--- ext/mysql.c
+++ ext/mysql.c
@@ -1078,9 +1078,9 @@ static VALUE fetch_hash2(VALUE obj, VALUE with_table)
 						/* To distinguish between binary and non-binary data for string data types, check whether the charsetnr value is 63.
 						   If so, the character set is binary, which indicates binary rather than non-binary data. */
 						if(fields[i].charsetnr == 63) {
-            	rb_hash_aset(hash, rb_ary_entry(colname, i), row[i]? rb_tainted_str_new(row[i], lengths[i]): Qnil);
+            	  rb_hash_aset(hash, rb_ary_entry(colname, i), row[i]? rb_tainted_str_new(row[i], lengths[i]): Qnil);
 						} else {
-							rb_hash_aset(hash, rb_ary_entry(colname, i), row[i]? rb_external_str_new_with_enc(row[i], lengths[i], rb_utf8_encoding()): Qnil);
+							  rb_hash_aset(hash, rb_ary_entry(colname, i), row[i]? rb_external_str_new_with_enc(row[i], lengths[i], rb_utf8_encoding()): Qnil);
 						}
         } else {
             rb_hash_aset(hash, rb_ary_entry(colname, i), row[i]? rb_external_str_new_with_enc(row[i], lengths[i], rb_utf8_encoding()): Qnil);
